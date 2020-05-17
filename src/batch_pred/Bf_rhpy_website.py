5247bf0fe9a367e5ce61bee0ad75c1d516e83d3c
vam-sin <vamsinallapareddy@gmail.com>
2020-05-17 11:19:35 +0530

    Renumber addition

--- src/batch_pred/Bf_rhpy_website.py
+++ src/batch_pred/Bf_rhpy_website.py
@@ -42,7 +42,30 @@ def get_bf_rhpy(pdb, res, chain):
 		print("New Menv Failed")
 
 	try:
-		print("Try psf")
+		print("Try Running Renumber")
+		renumber_script = os.path.join(PROJECT_PATH, 'menv_server', 'newrenumber.sh')
+		psf_file = PROJECT_PATH + 'PDB_Data/' + pdb + '-psf.menv'
+		cmd = 'csh ' + renumber_script + ' ' + pdbfile + ' ' + num_file 
+		print(cmd)
+		os.system(cmd)
+		result_file = os.path.join(PROJECT_PATH, 'PDB_Data', pdb + '-new.menv')
+		f = open(result_file, "r")
+		for x in f:
+			x = x.split(',')
+			residue_num = x[1].split(' ')
+			residue = int(residue_num[len(residue_num)-3])
+			atom = x[3]
+			ch = residue_num[len(residue_num)-6]
+			bf = float(x[4])
+			rhpy = float(x[7])
+
+			if (residue == res) and (ch == chain) and ((atom == 'TO') or (atom == 'TD')):
+				print("New Menv")
+				return bf, rhpy
+	except:
+		print("Renumber failed")
+
+	try:
 		result_file = os.path.join(PROJECT_PATH, 'PDB_Data', pdb + '-psf.menv')
 		f = open(result_file, "r")
 		for x in f:
